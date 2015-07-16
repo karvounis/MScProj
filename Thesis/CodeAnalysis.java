acf9ff1d6ea21549ecb8eeb2c3c336efbde24f49
Evangelos Karvounis <karvounis.b@gmail.com>
2015-07-16 23:02:13 +0100

    16/7 GUI update

--- Thesis/CodeAnalysis.java
+++ Thesis/CodeAnalysis.java
@@ -1,4 +1,21 @@
+import java.io.File;
 
 public class CodeAnalysis {
 
+	private File component;
+	
+	public CodeAnalysis(File component){
+		this.component = component;
+		System.err.println(component.getName());
+		System.err.println("Till here");
+		System.err.println(component.getAbsolutePath());
+	}
+	
+	public void startAnalysis(){
+		System.err.println("Start");
+	}
+	
+	public File getComponent(){
+		return component;
+	}
 }
--- Thesis/GUI.java
+++ Thesis/GUI.java
@@ -1,39 +1,69 @@
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileFilter;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class GUI extends JFrame implements ActionListener{
 
-	private JLabel text;
-	private JButton submitJar, clearJar, chooseJar;
-	private JFileChooser choice;
-	private JTextField log;
+	private JLabel nameJar;
+	private JButton start, clear, choose, exit;
+	private JFileChooser fileChooser;
+	private JTextArea log;
+	private JPanel top, center, bot;
+	private File component;	
+	private CodeAnalysis frida;
 
 	public GUI(){
-		super("Welcome!"); 
+		super("Welcome to my thesis GUI!"); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new GridBagLayout());
 		setSize(600, 400);
 
-		text = new JLabel("Choose a .jar file for analysis:");
-		getContentPane().add(text);
+		//North layout
+		top = new JPanel();		
+		top.add(new JLabel("Choose a .jar file for analysis:"));
 
-		chooseJar = new JButton("Open jar...");
-		chooseJar.addActionListener(this);
-		getContentPane().add(chooseJar);
+		//Opens the FileChooser
+		choose = new JButton("Open jar...");
+		choose.addActionListener(this);
 
-		log = new JTextField();
-		log.setSize(300, 200);
-		getContentPane().add(log);
-		//choice = new JFileChooser();
-		//getContentPane().add(choice);
+		//Clears the chosen component
+		clear = new JButton("Clear");
+		clear.addActionListener(this);
 
-		submitJar = new JButton("Submit");
+		nameJar = new JLabel();
+
+		top.add(choose);
+		top.add(clear);
+		top.add(nameJar);
+
+		add(top, BorderLayout.NORTH);
+
+		//Center Layout
+		center = new JPanel();
+
+		//Logging text area
+		log = new JTextArea(15, 30);
+
+		center.add(new JLabel("Log:"));
+		center.add(log);
+
+		add(center, BorderLayout.CENTER);
+
+		//South layout
+		bot = new JPanel();
+
+		start = new JButton("Start doing thingies");
+		start.addActionListener(this);
+		exit = new JButton("Exit");
+		exit.addActionListener(this);
+
+		bot.add(start);
+		bot.add(exit);
+
+		add(bot, BorderLayout.SOUTH);
 
 		setVisible(true);
 	}
@@ -41,39 +71,58 @@ public class GUI extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		if(e.getSource() == chooseJar){
-			choice = new JFileChooser();
+		if(e.getSource() == choose){
+			fileChooser = new JFileChooser();
 			//choice = new JFileChooser(System.getProperty("user.home"));
-			
+			//Accepts only .jar files and image
 			FileNameExtensionFilter type1 = new FileNameExtensionFilter("Image Files", new String[] {"jpg", "bmp", "png"});
 			FileNameExtensionFilter type2 = new FileNameExtensionFilter("Jar files", "jar");
-			
-			choice.setAcceptAllFileFilterUsed(false);
-			choice.addChoosableFileFilter(type2);
-			choice.addChoosableFileFilter(type1);
 
-			choice.setFileFilter(type2);
+			fileChooser.setAcceptAllFileFilterUsed(false);
+			fileChooser.addChoosableFileFilter(type2);
+			fileChooser.addChoosableFileFilter(type1);
+			fileChooser.setFileFilter(type2);
 
-			int returnVal = choice.showDialog(GUI.this,
-					"Choose");
+			int returnVal = fileChooser.showDialog(GUI.this, "Choose");
 
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				File file = choice.getSelectedFile();
-				//log.setText("Attaching file: " + file.getName()
-				//          + "." + "\n");
-				System.err.println(file.getName());
-				System.err.println("Till here");
-				System.err.println(file.getAbsolutePath());
+				component = fileChooser.getSelectedFile();
+				log.append("File chosen: " + component.getName() + ".\n");
+				//System.err.println(component.getName());
+				//System.err.println("Till here");
+				//System.err.println(component.getAbsolutePath());
 
+				nameJar.setText(component.getName());
 			} else {
-				//log.append("Attachment cancelled by user." + newline);
+				log.append("Jar choice cancelled by user.\n");
 			}
+		}else if(e.getSource() == start){
+			//Start button pressed
+			if(component != null){
+				log.append("Initiation! FIRE!\n");
+				frida = new CodeAnalysis(component);
+				frida.startAnalysis();
+			}else{
+				log.append("Choose jar please!\n");
+			}
+		}else if(e.getSource() == clear){
+			//Clear button pressed
+			if(component != null){
+				component = null;
+				log.append("Cleared selected jar.\n");
+				nameJar.setText("");
+			}
+		}else if(e.getSource() == exit){
+			//Exit button pressed
+			System.exit(0);
 		}
 	}
 
 	public static void main(String[] args) {
-
-		GUI john = new GUI();
-
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new GUI();
+			}
+		});
 	}
 }
